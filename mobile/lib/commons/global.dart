2733f75549d2323f38cbfabdb29132b604a0567e
S.Mahdi Hossaini <s.mahdi.hossaini@gmail.com>
2020-09-22 02:35:00 +0330

    added data model field

--- mobile/lib/commons/global.dart
+++ mobile/lib/commons/global.dart
@@ -1,8 +1,10 @@
 import 'package:epios/models/account.model.dart';
+import 'package:epios/models/data.model.dart';
 import 'package:epios/services/storage.service.dart';
 
 class Global{
   static final String version = "0.1.0";
   static AccountModel account;
   static StorageService storage = StorageService();
+  static DataModel data;
 }

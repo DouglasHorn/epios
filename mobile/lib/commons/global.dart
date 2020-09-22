b3e1a1b2bb7185ac63b57fa1cb6c67b13b3e2fc6
S.Mahdi Hossaini <s.mahdi.hossaini@gmail.com>
2020-09-22 22:40:57 +0330

    added contract service

--- mobile/lib/commons/global.dart
+++ mobile/lib/commons/global.dart
@@ -1,10 +1,12 @@
 import 'package:epios/models/account.model.dart';
 import 'package:epios/models/data.model.dart';
+import 'package:epios/services/contract.service.dart';
 import 'package:epios/services/storage.service.dart';
 
 class Global{
   static final String version = "0.1.0";
   static AccountModel account;
   static StorageService storage = StorageService();
+  static ContractService contractService = ContractService()..init();
   static DataModel data;
 }

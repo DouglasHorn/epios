9b2ad958f68b504d7a362a80dad0fc75e0491801
S.Mahdi Hossaini <s.mahdi.hossaini@gmail.com>
2020-09-29 22:57:09 +0330

    added actions

--- mobile/lib/pgaes/performTest.page/performTest.page.dart
+++ mobile/lib/pgaes/performTest.page/performTest.page.dart
@@ -1,16 +1,31 @@
-import 'package:epios/commons/styles.dart';
-import 'package:epios/components/simpleAppBar.component.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 
+import 'package:epios/commons/extension.dart';
+import 'package:epios/commons/global.dart';
+import 'package:epios/commons/styles.dart';
+import 'package:epios/components/inlineMessage.component.dart';
+import 'package:epios/components/simpleAppBar.component.dart';
+import 'package:epios/models/data.model.dart';
+import 'package:epios/services/contract.service.dart';
+
 class PerformTestPage extends StatefulWidget {
+  final TestModel model;
+  const PerformTestPage({
+    Key key,
+    @required this.model,
+  }) : super(key: key);
   @override
   _PerformTestPageState createState() => _PerformTestPageState();
 }
 
 class _PerformTestPageState extends State<PerformTestPage> {
   var _state = 0;
+  InlineMessageModel _message;
+  String _couponId;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +51,7 @@ class _PerformTestPageState extends State<PerformTestPage> {
                   padding: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
                   child: Text("Your test will have a card with a privacy foil covering your unique QR code. Scratch it off and scan your code.",style: textHeaderStyle,)
                 ),
+                InlineMessage(model: _message,),
                 SvgPicture.asset("assets/images/svgs/scanqr.svg"),
                 sized_20,
                 Spacer(),
@@ -51,10 +67,23 @@ class _PerformTestPageState extends State<PerformTestPage> {
                       textColor: outlineButtonBorderColor,
                       onPressed: ()async{
                         String cameraScanResult = await scanner.scan();
-                        print(cameraScanResult);
-                        setState(() {
-                          _state = 1;
-                        });
+                        if(cameraScanResult!=null){
+                          try {
+                            var hash = cameraScanResult.getCouponHash;
+                            _message = InlineMessageModel.info(message: "Validating coupon ...");
+                            setState(() {});
+                            var d = await Global.contractService.checkCoupon(hash.getLittleEndian);
+                            if(d.secretKeyHash != hash){
+                              _message = InlineMessageModel.error(message: "This coupon is not valid in database");
+                            }else{
+                              _couponId = cameraScanResult;
+                              _state = 1;
+                            }                            
+                          } catch (e) {
+                            _message = InlineMessageModel.error(message:"Invalid coupon code");
+                          }
+                        }
+                        setState(() {});
                       }, 
                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
                     ),
@@ -124,6 +153,8 @@ class _PerformTestPageState extends State<PerformTestPage> {
                       borderSide: BorderSide(color: outlineButtonBorderColor),
                       textColor: outlineButtonBorderColor,
                       onPressed: ()async{
+                        widget.model.couponId = _couponId;
+                        Global.storage.setData(Global.data);
                         Navigator.pop(context);
                       }, 
                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),

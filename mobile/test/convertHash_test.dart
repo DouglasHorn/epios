c144144616d760d860f8752344cbab6210dd2332
S.Mahdi Hossaini <s.mahdi.hossaini@gmail.com>
2020-09-29 22:58:29 +0330

    added decode coupon test

--- mobile/test/convertHash_test.dart
+++ mobile/test/convertHash_test.dart
@@ -16,4 +16,13 @@ void main(){
     expect(res2, "299e11a1c6e4b1d637632ba88623f17a0c294ab7b9fb3eed703ca09a20e603bb");
 
   });
+
+  test("Decode Coupon",(){
+    var h = "LTFWGPY2ROZNM".getCouponHash;
+    print(h);
+    
+    h = "6KVD6DK3TWYRM".getCouponHash;
+    print(h);
+    expect(h, "8f5a3e0f987f86d44b6682272bd27c1a761d8c55dc322524cbd1e8a538f6df97");
+  });
 }
